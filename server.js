25342070b9c523a3dfab6e33453b2c009f96bef5
Sky020 <2397860h@student.gla.ac.uk>
2020-06-13 16:05:11 +0100

    add(3): deploy

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 require('dotenv').config()
 const express = require("express");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
 
@@ -10,6 +12,14 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'pug');
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.route("/").get((req, res) => {
   //Change the response to render the Pug template
