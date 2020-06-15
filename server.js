1656c923a4a2963ae743eb853b05d69e1a8fb0fb
Sky020 <2397860h@student.gla.ac.uk>
2020-06-15 18:15:15 +0100

    add(8): stable deploy

--- server.js
+++ server.js
@@ -37,6 +37,13 @@ myDB(async (client) => {
   app.route('/profile').get(ensureAuthenticated, (req, res) => {
     res.render('pug/profile', { username: req.user.username });
   });
+  app.route('/logout').get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+  app.use((req, res, next) => {
+    res.status(404).type('text').send('Not Found');
+  });
 
   passport.serializeUser((user, done) => {
     done(null, user._id);
