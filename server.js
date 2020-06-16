92d449bd72d7177dd1c24a5b3684b48a6b781518
Sky020 <2397860h@student.gla.ac.uk>
2020-06-16 13:14:48 +0100

    add(12): stable deploy

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const passport = require('passport');
 const myDB = require('./connection');
 const ObjectID = require('mongodb').ObjectID;
 const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
 
 const app = express();
 
@@ -43,13 +44,14 @@ myDB(async (client) => {
     res.redirect('/');
   });
   app.route('/register').post((req, res, next) => {
+    const hash = bcrypt.hashSync(req.body.password, 12);
     myDataBase.findOne({ username: req.body.username }, function (err, user) {
       if (err) {
         next(err);
       } else if (user) {
         res.redirect('/');
       } else {
-        myDataBase.insertOne({ username: req.body.username, password: req.body.password }, (err, doc) => {
+        myDataBase.insertOne({ username: req.body.username, password: hash }, (err, doc) => {
           if (err) {
             res.redirect('/');
           } else {
@@ -82,7 +84,7 @@ myDB(async (client) => {
         console.log('User ' + username + ' attempted to log in.');
         if (err) { return done(err); }
         if (!user) { return done(null, false); }
-        if (password !== user.password) { return done(null, false); }
+        if (!bcrypt.compareSync(password, user.password)) { return done(null, false); }
         return done(null, user);
       });
     }
