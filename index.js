be2f643f2f1482add546f1bf4eaf05d1672d12da
Shaun Hamilton <2397860h@student.gla.ac.uk>
2020-08-10 14:57:23 +0000

    10 - Logging a User Out

--- index.js
+++ index.js
@@ -40,11 +40,20 @@ myDB(async client => {
 
   app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
     res.redirect('/profile');
-  })
+  });
 
   app.route('/profile').get(ensureAuthenticated, (req,res) => {
     res.render(process.cwd() + '/views/pug/profile', {username: req.user.username});
-  })
+  });
+
+  app.route('/logout').get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).type('text').send('Not Found');
+  });
 
   // Serialization and deserialization here...
   passport.serializeUser((user, done) => {
@@ -78,7 +87,7 @@ function ensureAuthenticated(req, res, next) {
     return next();
   }
   res.redirect('/');
-};
+}
 
 // app.listen out here...
 
