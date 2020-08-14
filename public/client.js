a37427afc465ad08f5ad1be56058f51e5e57e501
Shaun Hamilton <2397860h@student.gla.ac.uk>
2020-08-14 13:34:15 +0000

    17 - Set Up the Environment

--- public/client.js
+++ public/client.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
-
-
+    /*global io*/
+    let socket = io();
+    
 
     // Form submittion with new message in field with id 'm'
     $('form').submit(function () {
-        var messageToSend = $('#m').val();
+        let messageToSend = $('#m').val();
         //send message to server here?
         $('#m').val('');
         return false; // prevent form submit from refreshing page
