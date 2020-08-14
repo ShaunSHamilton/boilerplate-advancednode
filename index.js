88ac38472748e1e7c7744ea776f244216b35902a
Shaun Hamilton <2397860h@student.gla.ac.uk>
2020-08-14 13:50:28 +0000

    18, 19 - Handle a Disconnect

--- index.js
+++ index.js
@@ -35,8 +35,17 @@ myDB(async client => {
   routes(app, myDataBase);
   auth(app, myDataBase);
 
+  let currentUsers = 0;
   io.on('connection', socket => {
+    ++currentUsers;
+    io.emit('user count', currentUsers);
     console.log('A user has connected');
+    
+    socket.on('disconnect', () => {
+      console.log('A user has disconnected');
+      --currentUsers;
+      io.emit('user count', currentUsers);
+    });
   });
 
 }).catch((e) => {
